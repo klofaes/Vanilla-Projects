5f31c74e12fc205b89fcda69ed6788c16d19304a
klofaes <vs201100@gmail.com>
2020-12-14 16:38:11 +0530

    Add ejs as default view-engine

--- server.js
+++ server.js
@@ -4,10 +4,13 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-   res.send(`<h1>Hello World</h1>`);
+   res.render("index");
 });
 
 const port = process.env.PORT || 3000;
