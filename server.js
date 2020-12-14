7fbca7dc8d10d7f1c9fcdcc719d979992ebc9ad2
klofaes <vs201100@gmail.com>
2020-12-14 16:48:37 +0530

    Add required dependencies and boiler-plate stuff

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const https = require("https");
+const bodyParser = require("body-parser");
+const {static} = require("express");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+   res.render("index");
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+   console.log("Server currently running at " + port);
+});
