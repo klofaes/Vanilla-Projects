5706ab33648b9ded1d0ef0e66d142fb1f1004274
klofaes <vs201100@gmail.com>
2020-12-21 14:32:51 +0530

    Remove dependecies - https and body-parser

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const express = require("express");
-const https = require("https");
-const bodyParser = require("body-parser");
 
 const app = express();
 
 app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.json());
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
